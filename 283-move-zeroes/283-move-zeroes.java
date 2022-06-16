19c4d54275dbb3d31aa6d5c8ada62ecb416a5eb6
Sandra Binoy <36753008+sandrabinoy@users.noreply.github.com>
2022-06-16 13:47:48 +0530

    Time: 3 ms (31.67%), Space: 54.6 MB (59.82%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,19 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int zeroCtr = 0;
-        for (int i = 0, j = 0; i < nums.length; i++) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[j++] = nums[i];
-            } else {
-                zeroCtr++;
             }
-            if (nums.length - j == zeroCtr) {
-                while (j < nums.length) {
-                    nums[j++] = 0;
-                }
-                break;
-            }
+        }
+        
+        for (int i = j; i < nums.length; i++) {
+            nums[j++] = 0;
         }
         
     }
