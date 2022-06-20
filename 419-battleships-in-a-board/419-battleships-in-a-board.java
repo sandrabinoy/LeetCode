c6f86705ad62793b3250aa2cc2e9072ce98ae6fe
Sandra Binoy <36753008+sandrabinoy@users.noreply.github.com>
2022-06-20 23:16:24 +0530

    Time: 1 ms (90.59%), Space: 43.6 MB (77.43%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.java
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -4,21 +4,28 @@ class Solution {
         int numOfShips = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == '.') {
-                    continue;
+                if (board[i][j] == 'X') {
+                    sink(board, i , j);
+                    numOfShips++;
                 }
-                if (i > 0 && board[i-1][j] == 'X') {
-                    continue;
-                }
-                if (j > 0 && board[i][j-1] == 'X') {
-                    continue;
-                }
-                
-                numOfShips++;
             }
         }
         
         return numOfShips;
+        
+    }
+    
+    public void sink(char[][] board, int i, int j) {
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] == '.') {
+            return;
+        }
+        
+        board[i][j] = '.';
+        sink(board, i - 1, j);
+        sink(board, i + 1, j);
+        sink(board, i, j - 1);
+        sink(board, i, j + 1);
         
     }
 }
